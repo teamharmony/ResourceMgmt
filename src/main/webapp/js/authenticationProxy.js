5569e50af920f23c27dd3b0bcc125b910d742718
teamharmony <shankakode@gmail.com>
2015-06-08 21:48:42 +0530

    Tests for UI

--- src/main/webapp/js/authenticationProxy.js
+++ src/main/webapp/js/authenticationProxy.js
@@ -1,40 +1,52 @@
-window["host"] = "http://localhost:8080/ResourceMgmt";
-window["refresh_token"] = null;//get it from Local storage if exists
-window["clientId"] = "meetMePal";
-window["bearerToken"] = null;
-
-
-
-function loginWithPwd(username, password) {
-    return $.ajax( {
-	      	url: host.concat("/oauth/token?grant_type=password&clientId=" + clientId + "&username=" + username + "&password=" + password),
-	      	async: false,
-	      	type: 'GET'
-	 	}).done(loginSuccess)
-}
-
-
-function loginSuccess(o) {
-	bearerToken = o.value;
-	refresh_token = o.refreshToken.value;
+(function() {
 	
-	setTimeout(function(){
+	
+	function loginSuccess(o) {
+		var that = this;
+		window.bearerToken = o.value;
+		window.refresh_token = o.refreshToken.value;
+		//store the refreshToken in localStorage
+				
+		//This function ensures we always have a valid bearer token.
+		bearerTimeOut = setTimeOut(function(){
+			clearTimeout(bearerTimeout);
+			that.loginWithRefreshToken(window.refresh_token);
+		}, o.expiresIn * 60 - 100);
 		
-	}, o.expiresIn)
-}
+		//clear the timer if not cleared by unloading time.
+		window.onbeforeunload = function() {
+			clearTimeout(bearerTimeout);
+		};
+	}
 
-
-
-function loginWithRefreshToken(refreshToken, reTry, context, args, retRef) {
-    return $.ajax( {
-	      	url: host.concat("/oauth/token?grant_type=refresh_token&clientId=" + clientId + "&refresh_token=" + refreshToken),
+	function defaultErrorhandler(e) {
+		console.log("Login Error");
+	}
+	
+	AuthenticationProxy = function(hostUrl,  clientId, refreshTokenFailureCallback, passwordFailureCallback) {
+		this.hostUrl = hostUrl;
+		this.clientId = clientId;
+		this.refreshTokenFailureCallback = (typeof refreshTokenFailureCallback === "undefined")? defaultErrorhandler: refreshTokenFailureCallback;
+		this.passwordFailureCallback = (typeof passwordFailureCallback === "undefined")? defaultErrorhandler: passwordFailureCallback;
+	}
+	
+	AuthenticationProxy.prototype.loginWithPassword = function(username, password) {
+		return $.ajax( {
+	      	url: this.hostUrl.concat("/oauth/token?grant_type=password&clientId=" + this.clientId + "&username=" + username + "&password=" + password),
 	      	async: false,
-	      	type: 'GET'
-	 }).done(function(o) {
-		 loginSuccess(o);
-		 retRef = reTry.apply(context, args);
-	 });
-}
-
-
+	      	type: 'GET',
+	      	context: this
+	 	}).done(loginSuccess).fail(this.passwordFailureCallback);		
+	}
+	
+	AuthenticationProxy.prototype.loginWithRefreshToken = function(refreshToken) {
+	    return $.ajax( {
+		      	url: this.hostUrl.concat("/oauth/token?grant_type=refresh_token&clientId=" + this.clientId + "&refresh_token=" + refreshToken),
+		      	async: false,
+		      	type: 'GET'
+		 }).done(this.loginSuccess).fail(this.refreshTokenFailureCallback);
+	}
+	
+	
+})()
 
